9471072bc41acdea8613504188c89ed09ef5b60b
Mathisnplus1 <mathis.achats74@gmail.com>
2024-03-04 16:47:21 +0100

    final dqn

--- src/train.py
+++ src/train.py
@@ -22,29 +22,43 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 class ProjectAgent:
-    def __init__(self, config, model):
+    def __init__(self, config=None, model=None):
         device = "cuda"
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.device = device
-        self.nb_actions = config['nb_actions']
-        self.gamma = config['gamma'] if 'gamma' in config.keys() else 0.95
-        self.batch_size = config['batch_size'] if 'batch_size' in config.keys() else 100
-        buffer_size = config['buffer_size'] if 'buffer_size' in config.keys() else int(1e5)
-        self.memory = ReplayBuffer(buffer_size,device)
-        self.epsilon_max = config['epsilon_max'] if 'epsilon_max' in config.keys() else 1.
-        self.epsilon_min = config['epsilon_min'] if 'epsilon_min' in config.keys() else 0.01
-        self.epsilon_stop = config['epsilon_decay_period'] if 'epsilon_decay_period' in config.keys() else 1000
-        self.epsilon_delay = config['epsilon_delay_decay'] if 'epsilon_delay_decay' in config.keys() else 20
-        self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
-        self.model = model.to(device)
-        self.target_model = deepcopy(self.model).to(device)
-        self.criterion = config['criterion'] if 'criterion' in config.keys() else torch.nn.MSELoss()
-        lr = config['learning_rate'] if 'learning_rate' in config.keys() else 0.001
-        self.optimizer = config['optimizer'] if 'optimizer' in config.keys() else torch.optim.Adam(self.model.parameters(), lr=lr)
-        self.nb_gradient_steps = config['gradient_steps'] if 'gradient_steps' in config.keys() else 1
-        self.update_target_strategy = config['update_target_strategy'] if 'update_target_strategy' in config.keys() else 'replace'
-        self.update_target_freq = config['update_target_freq'] if 'update_target_freq' in config.keys() else 20
-        self.update_target_tau = config['update_target_tau'] if 'update_target_tau' in config.keys() else 0.005
+        if config is not None :
+            self.nb_actions = config['nb_actions']
+            self.gamma = config['gamma'] if 'gamma' in config.keys() else 0.95
+            self.batch_size = config['batch_size'] if 'batch_size' in config.keys() else 100
+            buffer_size = config['buffer_size'] if 'buffer_size' in config.keys() else int(1e5)
+            self.memory = ReplayBuffer(buffer_size,device)
+            self.epsilon_max = config['epsilon_max'] if 'epsilon_max' in config.keys() else 1.
+            self.epsilon_min = config['epsilon_min'] if 'epsilon_min' in config.keys() else 0.01
+            self.epsilon_stop = config['epsilon_decay_period'] if 'epsilon_decay_period' in config.keys() else 1000
+            self.epsilon_delay = config['epsilon_delay_decay'] if 'epsilon_delay_decay' in config.keys() else 20
+            self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
+            self.model = model.to(device)
+            self.target_model = deepcopy(self.model).to(device)
+            self.criterion = config['criterion'] if 'criterion' in config.keys() else torch.nn.MSELoss()
+            lr = config['learning_rate'] if 'learning_rate' in config.keys() else 0.001
+            self.optimizer = config['optimizer'] if 'optimizer' in config.keys() else torch.optim.Adam(self.model.parameters(), lr=lr)
+            self.nb_gradient_steps = config['gradient_steps'] if 'gradient_steps' in config.keys() else 1
+            self.update_target_strategy = config['update_target_strategy'] if 'update_target_strategy' in config.keys() else 'replace'
+            self.update_target_freq = config['update_target_freq'] if 'update_target_freq' in config.keys() else 20
+            self.update_target_tau = config['update_target_tau'] if 'update_target_tau' in config.keys() else 0.005
+        else :
+            state_dim = 6
+            n_action = 4
+            nb_neurons=512
+            DQN = torch.nn.Sequential(nn.Linear(state_dim, nb_neurons),
+                                      nn.ReLU(),
+                                      nn.Linear(nb_neurons, nb_neurons),
+                                      nn.ReLU(), 
+                                      nn.Linear(nb_neurons, nb_neurons),
+                                      nn.ReLU(), 
+                                      nn.Linear(nb_neurons, n_action)).to(self.device)
+
+            self.model = DQN
     
     def gradient_step(self):
         if len(self.memory) > self.batch_size:
